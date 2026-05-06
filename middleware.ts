e52c931d6e3c4d37cc7890e7011a907aafc89239
javier Palencia <javi.web23@gmail.com>
2026-05-06 12:12:20 -0400

    Fix studio access by excluding it from middleware

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ import {routing} from "./i18n/routing";
 const intlMiddleware = createMiddleware(routing);
 
 const blockedPrefixes = ["/studio", "/api/sanity", "/sanity"];
+const localeStudioPattern = /^\/(en|es|fr-CA)\/studio(?:\/|$)/;
 
 function isBlockedPath(pathname: string) {
   return blockedPrefixes.some(
@@ -13,12 +14,21 @@ function isBlockedPath(pathname: string) {
 }
 
 export default function middleware(request: NextRequest) {
-  if (isBlockedPath(request.nextUrl.pathname)) {
+  const {pathname} = request.nextUrl;
+  if (isBlockedPath(pathname)) {
     return NextResponse.next();
+  }
+  const localeStudioMatch = pathname.match(localeStudioPattern);
+  if (localeStudioMatch) {
+    const studioPath = pathname.replace(/^\/(en|es|fr-CA)/, "");
+    const url = new URL(studioPath || "/studio", request.url);
+    return NextResponse.redirect(url);
   }
   return intlMiddleware(request);
 }
 
 export const config = {
-  matcher: ["/((?!api|trpc|_next|_vercel|studio|sanity|.*\\..*).*)"],
+  matcher: [
+    "/((?!api|trpc|_next|_vercel|studio|en/studio|es/studio|fr-CA/studio|sanity|.*\\..*).*)",
+  ],
 };
